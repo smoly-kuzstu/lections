256e8205d53c329de4bb57f1d93e38b102204f36
smoly <smoly.kuzstu@gmail.com>
2014-02-13 00:58:01 +0700

    Add auto generated crud.

---
+++ src/Kuzstu/BlogBundle/Form/BlogEntryType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kuzstu\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class BlogEntryType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('view_count')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Kuzstu\BlogBundle\Entity\BlogEntry'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'kuzstu_blogbundle_blogentry';
+    }
+}
---
+++ src/Kuzstu/BlogBundle/Tests/Controller/BlogEntryControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kuzstu\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BlogEntryControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Go to the list view
+        $crawler = $client->request('GET', '/blogentry/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /blogentry/");
+
+        // Go to the show view
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code");
+    }
+
+    */
+}
